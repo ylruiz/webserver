c8c9dcb4727a5aefb86c245640f855ceb5042945
¨Yunet¨ <yunet.lruiz94@gmail.com>
2019-04-07 13:51:45 -0300

    update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,15 @@
+let queryName = "";
+
 // GET method route
 const getContact = (req, res) => {
-  var name = req.query.name;
-  if (name == undefined) {
-    name = "";
+  queryName = req.query.name;
+  if (queryName == undefined) {
+    queryName = "";
   } 
   var html =`<form action="/contact-us" method="post"> 
                Enter your name*: 
                <br> 
-               <input type="text" name="name" placeholder="Full name" value=${name}> 
+               <input type="text" name="name" placeholder="Full name" value=${queryName}> 
                <br> 
                Enter your email*: 
                <br> 
@@ -17,19 +19,34 @@ const getContact = (req, res) => {
                <br>
                <textarea name="message" placeholder="Your message"> </textarea>
                <br>
-               <button type="submit">Submit</button> 
+               <button>Submit</button> 
               </form>`;
     res.send(html);  
 }
 
 const postContact = (req, res) => {
-  let name = req.body.name;
+  let nameBody = req.body.name;
   let email = req.body.email;
   var html = "";
-  if (name == ""|| email == ""){
-    var html = 'Please, enter your name and your email' +
-               ' <br>' +
-               '<a href="/contact-us"">Try again.</a>';
+  if (nameBody == "" || email == undefined ){
+    html =`<form action="/contact-us" method="post"> 
+               Enter your name*: 
+               <br> 
+               <input type="text" name="name" placeholder="Full name" value=${queryName}> 
+               <br> 
+               Enter your email*: 
+               <br> 
+               <input type="email" name="emailAddress" placeholder="example@gmail.com"> 
+               <br>
+               Enter your message: 
+               <br>
+               <textarea name="message" placeholder="Your message"> </textarea>
+               <br>
+               <button> Submit </button> 
+              </form>`;
+    res.send(html);
+  } else {
+    html = "success";
     res.send(html);
   }
 };
