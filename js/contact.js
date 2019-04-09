f1cc673e1292ccc87acbcb99f85c0756167ed271
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-04-09 13:39:35 -0300

    Remove the now ignored directory node_modules

--- js/contact.js
+++ js/contact.js
@@ -1,50 +1,14 @@
-let queryName = "";
-
 // GET method route
-const getContact = (req, res) => {
-  queryName = req.query.name;
-  if (queryName == undefined) {
-    queryName = "";
-  } 
-  var html =`<form action="/contact-us" method="post"> 
-               Enter your name*: 
-               <br> 
-               <input type="text" name="name" placeholder="Full name" value=${queryName}> 
-               <br> 
-               Enter your email*: 
-               <br> 
-               <input type="email" name="emailAddress" placeholder="example@gmail.com"> 
-               <br>
-               Enter your message: 
-               <br>
-               <textarea name="message" placeholder="Your message"> </textarea>
-               <br>
-               <button>Submit</button> 
-              </form>`;
-    res.send(html);  
-}
+const getContact = (req, res) => { 
+  res.render('contact', { title: 'Hey', message: 'Hello there!' });
+}  
 
 const postContact = (req, res) => {
   let nameBody = req.body.name;
-  let email = req.body.emailAddress;
+  let email = req.body.email;
   var html = "";
-  if (nameBody == "" || email == undefined ){
-    html =`<form action="/contact-us" method="post"> 
-               Enter your name*: 
-               <br> 
-               <input type="text" name="name" placeholder="Full name" value=${queryName}> 
-               <br> 
-               Enter your email*: 
-               <br> 
-               <input type="email" name="emailAddress" placeholder="example@gmail.com"> 
-               <br>
-               Enter your message: 
-               <br>
-               <textarea name="message" placeholder="Your message"> </textarea>
-               <br>
-               <button> Submit </button> 
-              </form>`;
-    res.send(html);
+  if (nameBody == "" || email == "" ){
+    res.render('contact', { title: 'Hey', message: 'Hello there!' });
   } else {
     html = "success";
     res.send(html);
