94a5b84f6aec8237f8a0c372f7b169dd26c26497
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-04-13 16:59:34 -0300

    error message after submitting the form

--- js/login.js
+++ js/login.js
@@ -1,21 +1,19 @@
 // GET method Login
 const getLogin = (req, res) => { 
-    res.render('login');
+    let visibility = 'invisible';
+    res.render('login', {visibility: visibility});
 }  
   
 // POST method Login
 const postLogin = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
-    if (email == "" || password == "" ){
-      res.render('login', {email: "", password: ""});
-    } else  if (email === "hello@test.com" && password === "hello"){  
+    if (email === "hello@test.com" && password === "hello"){  
         let redirectUrl = "/admin/?email="+email;
         res.redirect(redirectUrl);
-        //res.render('admin', {email: email});
     } else {
-        let html = "success";
-        res.send(html);
+        let visibility = 'visible';
+        res.render('login', {visibility: visibility});
     }
 }
   
