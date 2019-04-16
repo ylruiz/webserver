9bfff32603ac1585f9b2c5ce3d9de4d8424e6d57
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-04-16 16:41:58 -0300

    other users can login with facebook

--- config/passport.js
+++ config/passport.js
@@ -12,14 +12,9 @@ passport.use(new FacebookStrategy({
     clientID: FACEBOOK_CLIENT_ID,
     clientSecret: FACEBOOK_CLIENT_SECRET,
     callbackURL: FACEBOOK_URL
-  }, (accessToken, refreshToken, profile, cb) => {
+  }, (accessToken, refreshToken, profile, done) => {
     console.log(profile);  
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
+    done(null, profile);
 }));
 
 // Configure the Google strategy for use by Passport.
